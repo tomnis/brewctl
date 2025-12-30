f6df321b453a4af1327442be2ada963a69fa0d89
Tomas McCandless <tomas.mccandless@gmail.com>
2025-12-30 00:28:55 -0600

    minor cleanup

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ def initialize_hardware() -> Tuple[Scale, Valve]:
         v: Valve= MockValve()
     return s, v
 
-scale, valve = initialize_hardware()
+my_scale, my_valve = initialize_hardware()
 
 @app.get("/")
 def read_root():
@@ -29,24 +29,25 @@ def read_root():
 
 @app.get("/scale")
 def read_weight():
-    weight = scale.get_weight()
-    battery_pct = scale.get_battery_percentage()
-    units = scale.get_units()
+    weight = my_scale.get_weight()
+    battery_pct = my_scale.get_battery_percentage()
+    units = my_scale.get_units()
     return {"weight": weight, "battery_pct": battery_pct, "units": units}  # Placeholder value in grams
 
 @app.post("/valve/step_forward")
 def step_forward():
-    valve.step_forward()
+    my_valve.step_forward()
     return {"status": "stepped forward"}  # Placeholder response
 
 @app.post("/valve/step_backward")
 def step_backward():
-    valve.step_backward()
+    my_valve.step_backward()
     return {"status": "stepped backward"}  # Placeholder response
 
+# TODO probably want to move breadcrumbs clientside and have an endpoint that steps a certain number of times
 @app.post("/valve/return_to_start")
 def return_to_start():
-    valve.return_to_start()
+    my_valve.return_to_start()
     return {"status": "returned to start"}  # Placeholder response
 
 
