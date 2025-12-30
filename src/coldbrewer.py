da739f11a4b9cdfbcdd4e7562c8c0c74677b2ef5
Tomas McCandless <tomas.mccandless@gmail.com>
2025-12-29 18:51:02 -0600

    initial commit

---
+++ src/coldbrewer.py
@@ -0,0 +1,144 @@
+import time
+from adafruit_motorkit import MotorKit
+from adafruit_motor import stepper
+from influxdb_client import InfluxDBClient, Point
+import os
+import math
+from statistics import mean
+from constants import *
+
+org = COLDBREW_INFLUXDB_ORG
+bucket_name = COLDBREW_INFLUXDB_BUCKET
+token = COLDBREW_INFLUXDB_TOKEN
+client = InfluxDBClient(url=COLDBREW_INFLUXDB_URL, token=token, org=org, timeout=30_000)
+
+kit = MotorKit()
+target_flow_rate = 0.05
+epsilon = 0.008
+
+
+initial_weight = 0
+is_first_time = True
+
+
+def get_current_weight(influx_client, org):
+    query_api = client.query_api()
+    query = 'from(bucket: "coldbrew")\
+            |> range(start: -10s)\
+            |> filter(fn: (r) => r._measurement == "coldbrew" and r._field == "weight_grams")'
+    tables = query_api.query(org=org, query=query)
+    for table in tables:
+        for record in table.records:
+            print(f"Time: {record.get_time()}, Value: {record.get_value()}")
+    result = tables[-1].records[-1]
+
+    # track our starting weight to derive a delta of when we should stop
+    if is_first_time:
+        is_first_time = False
+        initial_weight = result
+
+    return result.get_value()
+
+def get_flow_rate(influx_client, org):
+    query_api = client.query_api()
+    query = 'import "experimental/aggregate"\
+    from(bucket: "coldbrew")\
+      |> range(start: -2m)\
+      |> filter(fn: (r) => r._measurement == "coldbrew" and r._field == "weight_grams")\
+      |> aggregate.rate(every: 1m, unit: 1s)'
+    tables = query_api.query(org=org, query=query)
+    for table in tables:
+        for record in table.records:
+            print(f"Time: {record.get_time()}, Value: {record.get_value()}")
+
+    #results = [x.get_value() for x in tables[-1].records[-3:]]
+    #results = [x for x in results if x is not None]
+    result = tables[-1].records[-1]
+    #print(result)
+
+    #return mean(results)
+    return result.get_value()
+
+
+def flip_direction(direction):
+    if direction is stepper.FORWARD:
+        return stepper.BACKWARD
+    elif direction is stepper.BACKWARD:
+        return stepper.FORWARD
+    else:
+        raise RuntimeError(f"invalid direction {direction}")
+
+
+def directions_to_start(breadcrumbs):
+    # find which of the counts is larger, then return the reverse
+    max_direction = max(breadcrumbs, key=breadcrumbs.get)
+    opp = flip_direction(max_direction)
+    count = breadcrumbs[max_direction]
+    return (opp, count)
+
+def main():
+    """The main function of the script."""
+    breadcrumbs = dict()
+    interval = 60
+    #interval = 0.5
+    # target total weight, don't bother taring
+    #target_weight = 100 #1137
+    target_weight = 1137
+
+
+    # sleep to let the initial saturation drain
+    # TODO should add a param for this
+    #time.sleep(120)
+
+
+    # TODO move starting weight here
+
+    current_weight = 0
+
+    while current_weight < target_weight:
+        # get the current flow rate
+        print("====")
+        result = get_flow_rate(client, org)
+        print(f"got result: {result}")
+        if result is None:
+            print("result is none")
+            time.sleep(interval)
+            continue
+
+        elif abs(target_flow_rate - result) <= epsilon:
+            print("just right")
+            time.sleep(interval * 2)
+            continue
+        elif result <= target_flow_rate:
+            print("too slow")
+            direction = stepper.FORWARD
+        else:
+            print("too fast")
+            direction = stepper.BACKWARD
+
+        # update breadcrumbs so we can get back to starting position
+        if direction not in breadcrumbs:
+            breadcrumbs[direction] = 0
+        breadcrumbs[direction] += 1
+
+        kit.stepper1.onestep(direction=direction)
+        # sleep
+        time.sleep(interval)
+        #kit.stepper1.release()
+        #time.sleep()
+        current_weight = get_current_weight(client, org)
+        #current_weight += 1
+
+    # reached target weight, fully close the valve
+    print(f"reached target weight, breadcrumbs={breadcrumbs}")
+    (opp, count) = directions_to_start(breadcrumbs)
+
+    # rotate motor certain number of times
+    for i in range(count):
+        kit.stepper1.onestep(direction=opp)
+        time.sleep(0.1)
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/scale_collector.py
@@ -0,0 +1,67 @@
+from pyacaia import AcaiaScale
+from pyacaia import *
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+from retry import retry
+import pyacaia
+import time
+import influxdb_client
+import os
+from constants import *
+
+
+# TODO this should be async
+scale=AcaiaScale(mac=COLDBREW_SCALE_MAC_ADDRESS)
+scale.connect()
+
+# battery value in percent
+print("battery", scale.battery)
+
+# scale units is 'grams' or 'ounces'
+print("units", scale.units)
+
+# minutes of idle before auto-off
+print("auto-off", scale.auto_off)
+
+org = COLDBREW_INFLUXDB_ORG
+bucket_name = COLDBREW_INFLUXDB_BUCKET
+
+client = InfluxDBClient(url=COLDBREW_INFLUXDB_URL, token=COLDBREW_INFLUXDB_TOKEN, org=org, timeout=30_000)
+buckets_api = client.buckets_api()
+existing_bucket = buckets_api.find_bucket_by_name(bucket_name)
+if existing_bucket is None:
+    # Define retention rules (e.g., expire data after 7 days)
+    retention_rules = [influxdb_client.BucketRetentionRules(type="expire", every_seconds=604800)] # 7 days in seconds
+
+    # Create the bucket
+    created_bucket = buckets_api.create_bucket(bucket_name=bucket_name, retention_rules=retention_rules, org=org)
+    print(f"Bucket '{bucket_name}' created successfully.")
+else:
+    print(f"Bucket '{bucket_name}' already exists.")
+
+
+
+@retry(tries=10, delay=2)
+def write_scale_data(scale, client):
+    weight = scale.weight
+    print(scale.weight) # this is the property we can use to read the weigth in realtime
+
+    p = Point("coldbrew").field("weight_grams", weight).field("battery_pct", scale.battery)
+    client = InfluxDBClient(url=COLDBREW_INFLUXDB_URL, token=COLDBREW_INFLUXDB_TOKEN, org=org, timeout=30_000)
+    # TODO this should be async
+    write_api = client.write_api(write_options=SYNCHRONOUS)
+    write_api.write(bucket=bucket_name, record=p)
+
+
+#read and print the weight
+while(True):
+
+    write_scale_data(scale, client)
+    time.sleep(0.5)
+    # check if the scale is still connected, perhaps it was turned off?
+    # TODO there should be some more error handling around this
+    if not scale.connected:
+        break
+    # TODO should reconnect influxdbclient
+
+scale.disconnect()
