9f8096ed24f36edb97c22738a95748b315af037d
Tomas McCandless <tomas.mccandless@gmail.com>
2026-02-18 11:02:21 -0600

    allowed

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['coldbrewer.local'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
